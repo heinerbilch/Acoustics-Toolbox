1266b8c6040ced6d12bbe25de6e23cf3bf04a958
Heiner Strauß <heiner_ej@yahoo.de>
2024-05-24 19:41:09 +0200

    Trin strings for plots

--- Julia/plotray.jl
+++ Julia/plotray.jl
@@ -38,20 +38,8 @@ Nalpha = parse.(Int64, NBeamAngles[ 1 ])
 Nbeta  = parse.(Int64, NBeamAngles[ 2 ])
 
 # Extract letters between the quotes
-# nchars = strfind( TITLE, "''" );   # find quotes
-# TITLE  = [ TITLE[ nchars[ 1 ] + 1 : nchars[ 2 ] - 1 ] blanks( 7 - ( nchars[ 2 ] - nchars[ 1 ] ) ) ]
-# TITLE  = deblank( TITLE );  # remove whitespace
-
-# nchars = strfind( Type, "''" );   # find quotes
-# Type   = Type1[ nchars[ 1 ] + 1 : nchars[ 2 ] - 1 ]
-#Type  = deblank( Type );  # remove whitespace
-# read rays
-
-
-#xlabel( "Range [m]" )
-# if ( strcmp( units, "km" ) )
-#    xlabel( "Range [km]" )
-# end
+TITLE = lstrip(rstrip(replace(TITLE, "'" => "", count = 2)))
+PlotType = lstrip(rstrip(replace(PlotType, "'" => "", count = 2)))
 
 # axis limits
 rmin = +1e9
@@ -60,7 +48,7 @@ rmax = -1e9
 zmin = +1e9
 zmax = -1e9
 
-# this could be changed to a forever loop
+# read rays
 for isz = 1 : Nsz
    plot(title = TITLE, yflip = true, xlabel = "Range [m]", ylabel = "Depth [m]", legend = false, reuse = false)
    for ibeam = 1 : Nalpha
@@ -69,38 +57,40 @@ for isz = 1 : Nsz
       nsteps    = parse.(Int64, steps[1])
       NumTopBnc = parse.(Int64, steps[2])
       NumBotBnc = parse.(Int64, steps[3])
+
       r = fill(NaN, nsteps)
       z = fill(NaN, nsteps)
       for i in 1:nsteps
-#        switch PlotType
-#          case "rz"
-            ray = split(readline(fid))
-            r[i], z[i] = parse.((Float64, Float64), ray)
-#          case "xyz"
+        ray = split(readline(fid))
+        if cmp(PlotType, "rz") == 0
+          r[i], z[i] = parse.((Float64, Float64), ray)
+        elseif cmp(PlotType, "xyz") == 0
+          println("Not yet implemented. Exiting")
+          exit()
+# Matlab
 #             ray = fscanf( fid, "#f", [3 nsteps] )
-            
 #             xs = ray[ 1, 1 ]
 #             ys = ray[ 2, 1 ]
 #             r = sqrt( ( ray[ 1, : ] - xs ).^2 + ( ray[ 2, : ] - ys ).^2 )
 #             z = ray[ 3, : ]
-#        end # switch
+        else
+          println("Unknown Plottype. Exiting")
+          exit()
+        end # Plottype
       end # nsteps
       
-#       if ( strcmp( units, "km" ) )
-#          r = r / 1000;   # convert to km
-#       end
-      
       if NumTopBnc >= 1 && NumBotBnc >= 1
-         plot!(r, z, lc=:black )    # hits both boundaries
+         plot!(r, z, lc=:black) # hits both boundaries
       elseif NumBotBnc >= 1
-         plot!(r, z, lc=:blue )	   # hits bottom only
+         plot!(r, z, lc=:blue)	# hits bottom only
       elseif NumTopBnc >= 1
-         plot!(r, z, lc=:green )	   # hits surface only
+         plot!(r, z, lc=:green) # hits surface only
       else()
-         plot!(r, z, lc=:red )    # direct path()
+         plot!(r, z, lc=:red)   # direct path()
       end
       
       # update axis limits
+# Matlab
 #      rmin = min( [ r rmin ] )
 #      rmax = max( [ r rmax ] )
 
@@ -119,7 +109,7 @@ for isz = 1 : Nsz
 #      end
 
    end	# next beam
-gui()
+gui() # Plot every source depth seperate
 end # next source depth
 end # read loop
 
